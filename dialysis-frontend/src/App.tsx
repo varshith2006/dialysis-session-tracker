5ab2bb97ec838c2bfc20e57a28c9ab29108351cb
varshith2006 <166216668+varshith2006@users.noreply.github.com>
2026-03-20 15:35:51 +0530

    Create App component for Dialysis Dashboard

---
+++ dialysis-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Dialysis Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
